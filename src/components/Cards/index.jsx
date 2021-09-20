b764800b3213b5d8d215837453d08255c2cdf3a7
Arthur Sarmento <arthurbs17@gmail.com>
2021-09-20 18:02:46 -0300

    Funcionalidade list add

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,10 +1,38 @@
+import * as React from "react";
+import {
+  List,
+  ListItem,
+  Divider,
+  ListItemText,
+  ListItemAvatar,
+  Avatar,
+  Typography,
+} from "@material-ui/core";
+
 const Cards = ({ list }) => {
   return (
-    <ul>
-      {list.map((repo) => (
-        <li>{repo.full_name}</li>
+    <List>
+      {list.map((repo, index) => (
+        <ListItem alignItems="flex-start" key={index}>
+          <ListItemAvatar>
+            <Avatar src={repo.organization.avatar_url} alt={repo.full_name} />
+          </ListItemAvatar>
+          <ListItemText
+            primary={repo.full_name}
+            secondary={
+              <React.Fragment>
+                <Typography
+                  sx={{ display: "inline" }}
+                  component="span"
+                  variant="body2"
+                ></Typography>
+                {repo.description}
+              </React.Fragment>
+            }
+          />
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
